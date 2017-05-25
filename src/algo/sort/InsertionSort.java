d1678d6d3b86c927b10117a2fc70212c45c6ef9d
Michele Orsi <michele.orsi@lastminute.com>
2017-05-25 17:48:49 +0200

    added insertion sort

--- src/algo/sort/InsertionSort.java
+++ src/algo/sort/InsertionSort.java
@@ -1,8 +1,35 @@
 package algo.sort;
 
-/**
- * Created by morsi on 04/05/2017.
- */
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class InsertionSort
 {
+  @Test
+  public void test()
+  {
+    int[] arr = {2,1,6,4,0,3};
+
+    sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  public void sort(int[] array)
+  {
+    for(int i=1; i<array.length; i++)
+    {
+      for(int j=0; j<i; j++)
+      {
+        if(array[i]<array[j])
+        {
+          int temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+
+    }
+
+  }
 }
