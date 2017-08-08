6e7cc30d689dbd390ef26f173e7de27af29d74d1
Michele Orsi <me@micheleorsi.it>
2017-08-08 14:10:25 +0100

    some improvements

--- src/algo/linkedlist/PartitionLinkedList.java
+++ src/algo/linkedlist/PartitionLinkedList.java
@@ -7,47 +7,42 @@ public class PartitionLinkedList
   @Test
   public void test()
   {
-    Node<Integer> one = new Node(1);
-    Node<Integer> three = new Node(3);
-    Node<Integer> five = new Node(5);
-    Node<Integer> four = new Node(4);
-    Node<Integer> two = new Node(2);
-    one.next = three;
-    three.next = five;
-    five.next = four;
-    four.next = two;
+    LLNode five = new LLNode(5,null);
+    LLNode four = new LLNode(4, five);
+    LLNode three = new LLNode(3,four);
+    LLNode two = new LLNode(2,three);
+    LLNode one = new LLNode(1,two);
 
-    Node actual = one;
+    LLNode actual = one;
     while(actual!=null)
     {
-      System.out.println(actual.data);
+      System.out.println(actual.item);
       actual = actual.next;
     }
 
-    Node head = partition(one,4);
+    LLNode head = partition(one,4);
     System.out.println("--- PARTITION ---");
 
     actual = head;
     while(actual!=null)
     {
-      System.out.println(actual.data);
+      System.out.println(actual.item);
       actual = actual.next;
     }
-
   }
 
-  private Node partition(Node<Integer> node, int val)
+  private LLNode partition(LLNode node, int val)
   {
-    Node beforeStart = null;
-    Node beforeEnd = null;
-    Node afterStart = null;
-    Node afterEnd = null;
+    LLNode beforeStart = null;
+    LLNode beforeEnd = null;
+    LLNode afterStart = null;
+    LLNode afterEnd = null;
 
     while(node!=null)
     {
-      Node next = node.next;
+      LLNode next = node.next;
       node.next = null;
-      if(node.data<val)
+      if(node.item<val)
       {
         if(beforeStart==null)
         {
