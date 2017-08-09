46efbaaf4c59e99264e54e322276d3c3d2c4963f
Michele Orsi <me@micheleorsi.it>
2017-08-09 13:26:50 +0100

    changed layout

--- src/algo/sort/MergeSort.java
+++ src/algo/sort/MergeSort.java
@@ -11,38 +11,36 @@ public class MergeSort implements Sort
     }
 
     // Main function that sorts arr[l..r] using mergesort()
-    private void mergesort(int arr[], int left, int right)
-    {
-        if (left < right)
-        {
-            int middle = (left+right)/2; // Find the middle point
-            mergesort(arr, left, middle); // Sort first and second halves
-            mergesort(arr , middle+1, right); // Merge the sorted halves
+    private void mergesort(int arr[], int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int middle = (left+right)/2;
+            // Sort first and second halves
+            mergesort(arr, left, middle);
+            mergesort(arr , middle+1, right);
+            // Merge the sorted halves
             merge(arr, left, middle, right);
         }
     }
 
-    private void merge(int arr[], int left, int middle, int right)
-    {
+    private void merge(int arr[], int left, int middle, int right) {
         Queue<Integer> lQueue = new LinkedList<>();
         Queue<Integer> rQueue = new LinkedList<>();
         for (int i = left; i<= middle; i++) // O(n)
             lQueue.add(arr[i]);
         for (int j = middle+1; j<= right; j++) // O(n)
             rQueue.add(arr[j]);
+
         int i = left;
-        while(!(lQueue.isEmpty() || rQueue.isEmpty())) // O(n)
-        {
-            if(lQueue.peek() <= rQueue.peek())
-            {
+        while(!(lQueue.isEmpty() || rQueue.isEmpty())) { // O(n)
+            if(lQueue.peek() <= rQueue.peek()) {
                 arr[i++]=lQueue.poll();
-            }
-            else
-            {
+            } else {
                 arr[i++]=rQueue.poll();
             }
         }
         while(!lQueue.isEmpty()) arr[i++]= lQueue.poll(); // O(n)
         while(!rQueue.isEmpty()) arr[i++]= rQueue.poll(); // O(n)
     }
+
 }
