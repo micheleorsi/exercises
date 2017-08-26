d00efeceb07e1a36d10a027db1ec7607b986f069
Michele Orsi <me@micheleorsi.it>
2017-08-26 12:41:34 +0200

    bfs with cycle and without cycle

--- src/algo/graph/BreadFirstSearch.java
+++ src/algo/graph/BreadFirstSearch.java
@@ -6,19 +6,68 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- *                  1
- *                  |
- *            |-5 - 2 - 6
- *            |     |
- *            |     3
- *            |    / \
- *            |---4  7
- */
 public class BreadFirstSearch
 {
+  /**
+   *                  1
+   *                  |
+   *              5 - 2 - 6
+   *                  |
+   *                  3
+   *                 / \
+   *                4  7
+   */
   @Test
-  public void test()
+  public void testWithoutCycle() {
+    Graph<GNode> g = Graph.Builder.buildWithoutCycle();
+    g.init();
+
+    for(GNode node: g.nodes)
+    {
+      Assert.assertEquals(GNode.Status.UNDISCOVERED,node.status);
+    }
+    List<GNode> nodeSeq = new LinkedList<>();
+    List<String> edgeSeq = new LinkedList<>();
+
+    BFS bfs = new BFS(nodeSeq,edgeSeq);
+
+    bfs.search(g,g.nodes.get(0));
+
+    Assert.assertEquals(1, nodeSeq.get(0).value);
+    Assert.assertEquals(2, nodeSeq.get(1).value);
+    Assert.assertEquals(5, nodeSeq.get(2).value);
+    Assert.assertEquals(3, nodeSeq.get(3).value);
+    Assert.assertEquals(6, nodeSeq.get(4).value);
+    Assert.assertEquals(4, nodeSeq.get(5).value);
+    Assert.assertEquals(7, nodeSeq.get(6).value);
+
+    Assert.assertEquals(6, edgeSeq.size());
+
+    Assert.assertEquals("1-2", edgeSeq.get(0));
+    Assert.assertEquals("2-5", edgeSeq.get(1));
+    Assert.assertEquals("2-3", edgeSeq.get(2));
+    Assert.assertEquals("2-6", edgeSeq.get(3));
+    Assert.assertEquals("3-4", edgeSeq.get(4));
+    Assert.assertEquals("3-7", edgeSeq.get(5));
+
+    for(GNode node: g.nodes)
+    {
+      Assert.assertEquals(GNode.Status.PROCESSED,node.status);
+    }
+
+  }
+
+  /**
+   *                  1
+   *                  |
+   *            |-5 - 2 - 6
+   *            |     |
+   *            |     3
+   *            |    / \
+   *            |---4  7
+   */
+  @Test
+  public void testWithCycle()
   {
     Graph<GNode> g = Graph.Builder.buildWithCycle();
     g.init();
